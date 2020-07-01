03e348c41a2a52b10cf94f8bc500a21b88d3f2c0
BrunoFaria11 <bruno.eduardo.faria99@gmail.com>
2020-07-01 21:52:29 +0100

    First Commit

---
+++ Ecommerce_Primos/Ecommerce_MVC_Core/Migrations/20200701181214_add_Enitys_to_Man_WebSite_mig.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ecommerce_MVC_Core.Migrations
+{
+    public partial class add_Enitys_to_Man_WebSite_mig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Category_Category_CategoryId",
+                table: "Category");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Category_CategoryId",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "Category");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Category",
+                newName: "Image_Path");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AddedDate",
+                table: "ProductLikes",
+                nullable: false,
+                defaultValue: new DateTime(2020, 7, 1, 19, 12, 14, 218, DateTimeKind.Local),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2020, 4, 21, 0, 3, 21, 364, DateTimeKind.Local));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AddedDate",
+                table: "OrderStatus",
+                nullable: false,
+                defaultValue: new DateTime(2020, 7, 1, 19, 12, 14, 241, DateTimeKind.Local),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2020, 4, 21, 0, 3, 21, 411, DateTimeKind.Local));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Category",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Category");
+
+            migrationBuilder.RenameColumn(
+                name: "Image_Path",
+                table: "Category",
+                newName: "Description");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AddedDate",
+                table: "ProductLikes",
+                nullable: false,
+                defaultValue: new DateTime(2020, 4, 21, 0, 3, 21, 364, DateTimeKind.Local),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2020, 7, 1, 19, 12, 14, 218, DateTimeKind.Local));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AddedDate",
+                table: "OrderStatus",
+                nullable: false,
+                defaultValue: new DateTime(2020, 4, 21, 0, 3, 21, 411, DateTimeKind.Local),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2020, 7, 1, 19, 12, 14, 241, DateTimeKind.Local));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Category",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Category",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_CategoryId",
+                table: "Category",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Category_Category_CategoryId",
+                table: "Category",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ Ecommerce_Primos/Ecommerce_MVC_Core/ViewModel/CategoryViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Ecommerce_MVC_Core.ViewModel
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Image_Path { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+    }
+
+    public class CategoryListViewModel
+    {
+        public int Id { get; set; }
+        public string Image_Path { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+    }
+}

78fd97547aa6e597da178e547140520235cd72cd
BrunoFaria11 <bruno.eduardo.faria99@gmail.com>
2020-07-07 22:26:52 +0100

    Fix Generic Modal

--- Controllers/Admin/AdminController.cs
+++ Controllers/Admin/AdminController.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Ecommerce_MVC_Core.Models.Admin;
 using Ecommerce_MVC_Core.Repository;
 using Ecommerce_MVC_Core.ViewModel;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Nancy.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Ecommerce_MVC_Core.Controllers.Admin
@@ -35,9 +37,41 @@ namespace Ecommerce_MVC_Core.Controllers.Admin
 
         public IActionResult WebSite()
         {
+            ViewBag.Category = _unitOfWork.Repository<Category>().GetAll().ToList();
+
             return View();
         }
 
 
+
+
+        public string UploadImages()
+        {
+            string result = string.Empty;
+            try
+            {
+                long size = 0;
+                var file = Request.Form.Files;
+
+                var filename = ContentDispositionHeaderValue.Parse(file[0].ContentDisposition).Name.Trim('"');
+                string FilePath = Path.Combine(_hosingEnv.WebRootPath, "css/Images/");
+                var file_ = Path.Combine(FilePath, filename + ".jpg");
+
+                size += file[0].Length;
+
+                using (FileStream fs = System.IO.File.Create(file_))
+                {
+
+                    file[0].CopyTo(fs);
+                    fs.Flush();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                result = ex.Message;
+            }
+            return result;
+        }
     }
 }
